b782afb28be49e2e4ae81fc60b2be3309d219b35
Luccas Clezar <luccas.clezar@gmail.com>
2024-10-24 23:00:38 -0300

    feat: prevent 'clickable' cursor on macOS
    Flutter changes the cursor to 'clickable' when hovering over interactive
    elements by default, which is not the native behavior on macOS.

--- README.md
+++ README.md
@@ -1,16 +1,69 @@
-# soundnexus
+# SoundNexus
 
-A new Flutter project.
+SoundNexus is a multiplatform soundboard app made with Flutter to use with
+podcasts, tabletop RPGs, or any other activity that you need to control dozens
+of sounds simultaneously.
 
-## Getting Started
+## Architecture
 
-This project is a starting point for a Flutter application.
+The project's organization is based on CodeWithAndrea's Riverpod Architecture.
 
-A few resources to get you started if this is your first Flutter project:
+For more information on how it works, read [Flutter App Architecture with Riverpod: An Introduction](https://codewithandrea.com/articles/flutter-app-architecture-riverpod-introduction/).
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+<br>
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# Modifications to flutter
+
+## Updating DesktopMouseTracker
+
+DesktopMouseTracker is a class that prevents the cursor from becoming a pointer
+when hovering buttons (as it is natively) on macOS.
+
+This class is actually copied almost in its entirety from Flutter's
+MouseTracker file, but with a simple change that prevents the pointer cursor.
+
+To update the DesktopMouseTracker class, follow these instructions:
+
+1. Copy the whole `mouse_tracker.dart` file from Flutter
+(`flutter/lib/source/rendering/mouse_tracker.dart`) to
+`desktop_mouse_tracker.dart`.
+
+2. Add these ignores as the first line:
+
+```dart
+// ignore_for_file: lines_longer_than_80_chars, omit_local_variable_types, comment_references, prefer_collection_literals, require_trailing_commas, prefer_asserts_with_message, cascade_invocations
+```
+
+3. Rename `MouseTracker` to `DesktopMouseTracker`.
+
+4. Change the super class from `ChangeNotifier` to `MouseTracker` (import
+`flutter/rendering.dart`).
+
+5. Instead of using the super parameter on the `MouseTracker` ctor, use a
+static method like this:
+
+```dart
+DesktopMouseTracker()
+  : _hitTestInView = defaultHitTestInView,
+    super(defaultHitTestInView);
+
+static HitTestResult defaultHitTestInView(Offset position, int viewId) {
+  final result = HitTestResult();
+  RendererBinding.instance.hitTestInView(result, position, viewId);
+  return result;
+}
+```
+
+6. Add `@overrides` (saving should add them automatically).
+
+7. Find `_handleDeviceUpdate` method and update the callback for the cursor
+type to this:
+
+```dart
+details.nextAnnotations.keys.map(
+  (MouseTrackerAnnotation annotation) =>
+      annotation.cursor != SystemMouseCursors.click
+          ? annotation.cursor
+          : SystemMouseCursors.basic,
+),
+```
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flex_seed_scheme/flex_seed_scheme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:go_router/go_router.dart';
 import 'package:soundnexus/features/project/presentation/project_page.dart';
 import 'package:soundnexus/features/projects/presentation/projects_page.dart';
+import 'package:soundnexus/global/desktop_mouse_tracker.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 BuildContext get navigatorContext => navigatorKey.currentContext!;
@@ -19,26 +21,8 @@ Future<void> main() async {
   runApp(const ProviderScope(child: SoundNexusApp()));
 }
 
-class SoundNexusApp extends StatelessWidget {
+class SoundNexusApp extends StatefulWidget {
   const SoundNexusApp({super.key});
-
-  static GoRouter router = GoRouter(
-    navigatorKey: navigatorKey,
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const ProjectsPage(),
-        routes: [
-          GoRoute(
-            path: 'project/:projectId',
-            builder: (context, state) => ProjectPage(
-              projectId: state.pathParameters['projectId']!,
-            ),
-          ),
-        ],
-      ),
-    ],
-  );
 
   static ThemeData createTheme(Color seed, [Brightness? brightness]) {
     const primaryChroma = 64.0;
@@ -75,12 +59,47 @@ class SoundNexusApp extends StatelessWidget {
   }
 
   @override
+  State<SoundNexusApp> createState() => _SoundNexusAppState();
+}
+
+class _SoundNexusAppState extends State<SoundNexusApp> {
+  static final GoRouter _router = GoRouter(
+    navigatorKey: navigatorKey,
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProjectsPage(),
+        routes: [
+          GoRoute(
+            path: 'project/:projectId',
+            builder: (context, state) => ProjectPage(
+              projectId: state.pathParameters['projectId']!,
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    // On macOS the pointer shouldn't change to 'clickable' when hovering
+    // buttons. Overriding MouseTracker is the workaround.
+    if (Platform.isMacOS && !kIsWeb) {
+      // ignore: invalid_use_of_visible_for_testing_member
+      RendererBinding.instance.initMouseTracker(DesktopMouseTracker());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'SoundNexus',
-      theme: createTheme(Colors.indigo),
+      theme: SoundNexusApp.createTheme(Colors.indigo),
       scrollBehavior: const ScrollBehavior().copyWith(scrollbars: false),
-      routerConfig: router,
+      routerConfig: _router,
       builder: (context, child) => Listener(
         onPointerDown: (event) {
           // Handle mouse back button. Don't pop if web, as the
